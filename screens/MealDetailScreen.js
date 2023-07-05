719a3241b9e474f39646fc2ea71a2258b4ab38af
Prabodashan <prabodashan14@gmail.com>
2023-07-05 22:22:55 +0530

    Added an Icon Button to a Header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,14 +1,34 @@
+import { useLayoutEffect } from "react";
 import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 
 import List from "../components/MealDetail/List";
 import Subtitle from "../components/MealDetail/Subtitle";
 import MealDetails from "../components/MealDetails";
 import { MEALS } from "../data/dummy-data";
+import IconButton from "../components/IconButton";
 
 function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  function headerButtonPressHandler() {
+    console.log("Pressed!");
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon="star"
+            color="white"
+            onPress={headerButtonPressHandler}
+          />
+        );
+      },
+    });
+  }, [navigation, headerButtonPressHandler]);
 
   return (
     <ScrollView style={styles.rootContainer}>
@@ -32,31 +52,30 @@ function MealDetailScreen({ route }) {
   );
 }
 
-
 const styles = StyleSheet.create({
-    rootContainer: {
+  rootContainer: {
     marginBottom: 32,
-},
+  },
   image: {
-      width: "100%",
-      height: 350,
-    },
-    title: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
     fontWeight: "bold",
     fontSize: 24,
     margin: 8,
     textAlign: "center",
     color: "white",
-},
+  },
   detailText: {
-      color: "white",
-    },
-    listOuterContainer: {
-        alignItems: "center",
-    },
+    color: "white",
+  },
+  listOuterContainer: {
+    alignItems: "center",
+  },
   listContainer: {
     width: "80%",
-},
+  },
 });
 
 export default MealDetailScreen;
